34e86aaf52aa76c7ce426e9cb911ab85ae659560
Moacir Moda <moa.moda@gmail.com>
2017-01-10 10:56:48 -0200

    removing

--- wp-content/themes/bvs-hpcs/static/js/functions.js
+++
@@ -1,6 +0,0 @@
-jQuery = $;
-jQuery(document).ready(function($){
-    $("#searchForm").addClass('form');
-    $("#searchForm input[type=text]").addClass('form-control');
-    $("#searchForm input[type=submit]").addClass('form-control');
-});
